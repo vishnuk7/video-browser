8f307ece047dc438f0ac5350c9467c72caaa8ccb
viu <vishnu.kraman88@gmail.com>
2020-03-29 03:17:08 +0530

    search bar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
   <div id="app">
-    <p>Hello</p>
+    <SearchBar @termChange="onTermChange"></SearchBar>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
+import SearchBar from "./components/SearchBar";
+
+const API_KEY = process.env.VUE_APP_API_KEY;
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    SearchBar
+  },
+  methods: {
+    onTermChange(searchTerm) {
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm
+          }
+        })
+        .then(res => console.log(res));
+    }
+  }
 };
 </script>
 
