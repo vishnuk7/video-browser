ad245211b921bc5c26ca19df4793964abc3b1db5
viu <vishnu.kraman88@gmail.com>
2020-03-30 01:50:32 +0530

    video list item component

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,10 +1,17 @@
 <template>
-  <ul>{{ videos.length }}</ul>
+  <ul>
+    <VideoListItem v-for="video in videos" :video="video" :key="video.etag"></VideoListItem>
+  </ul>
 </template>
 
 <script>
+import VideoListItem from "./VideoListItem";
+
 export default {
   name: "VideoList",
-  props: ["videos"]
+  props: ["videos"],
+  components: {
+    VideoListItem
+  }
 };
 </script>
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <img :src="thumbnails" />
+    {{ video.snippet.title }}
+  </li>
+</template>
+
+<script>
+export default {
+  name: "VideoListItem",
+  props: ["video"],
+  computed: {
+    thumbnails() {
+      return this.video.snippet.thumbnails.default.url;
+    }
+  }
+};
+</script>
